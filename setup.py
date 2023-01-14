9d75751cade29b860665a16fd23c5320fab0c343
Alejandro <aledrums@gmail.com>
2023-01-14 01:35:31 -0500

    Updated setup

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(
     name="gym_environments",
     version="0.0.1",
     packages=setuptools.find_packages(),
+    package_dir={"": "envs"},
+    include_package_data=True,
     install_requires=["gym==0.26.2", "numpy==1.23.5", "pygame", "box2d-py"],
 )
